a75ab3a1ee66ebcb00bd1746f267b76fdca4f310
tarunrana0222 <tarunrana0222@gmail.com>
2021-02-01 16:22:11 +0530

    Share link

--- public/scripts/show.js
+++ public/scripts/show.js
@@ -7,3 +7,18 @@ $(()=>{
     })
 })
 
+$(() => { 
+    var $temp = $("<input>");
+var $url = $(location).attr("href");
+
+$("#copyLink").on("click", function () {
+  $("body").append($temp);
+  $temp.val($url).select();
+  document.execCommand("copy");
+    $temp.remove();
+    $("#copyLink").text(" Link Copied.!!");
+    $("#copyLink").removeClass('btn btn-outline-danger').addClass('btn btn-outline-success disabled');
+  
+});
+
+})
